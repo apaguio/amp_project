955380ae309788c8a52a2de88d4adf1304c05558
sherif <sherif@elementary-test.(none)>
2014-09-12 19:17:06 +0200

    Initial commit: basic architecture + implementation for ekm collector

---
+++ settings.py
@@ -0,0 +1,5 @@
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'US/Pacific'
