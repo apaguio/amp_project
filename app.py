272c2f205a57a9e069310d178c80a44e524e1143
Sherif Zain <sherifzain@yahoo.com>
2014-09-15 01:06:30 +0200

    Added: implementation for get_tarrif_details

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+from flask_settings import *
 from flask import Flask, Blueprint, abort, jsonify, request, session
 from influxdb_factory import get_influxdb
+from flask.ext.pymongo import PyMongo
 from tasks import add
 
 app = Flask(__name__)
+app.config.from_object('flask_settings')
 influxdb = get_influxdb()
+mongodb = PyMongo(app)
 
 @app.route("/test")
 def hello_world(x=16, y=16):
@@ -15,5 +19,11 @@ def hello_world(x=16, y=16):
     goto = "{}".format(context['id'])
     return jsonify(body=result, goto=goto, result=res.get())
 
+@app.route('/tarrif/<name>')
+def get_tarrif_details(name):
+    # later on, can be extended by periods
+    result = mongodb.db.tarrif.find_one({'name': name}, fields={'_id': False}) or list()
+    return jsonify(result)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
