de6677b3b2ae3222aa5b8b86a7eef2f3f15a6d10
Diaa Kasem <me@diaa.me>
2014-09-16 05:18:10 +0300

    Adding helpers and updating app.py

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask_settings import *
 from flask import Flask, Blueprint, abort, jsonify, request, session
 from influxdb_factory import get_influxdb
 from flask.ext.pymongo import PyMongo
+from helpers import CreateResponse
 
 app = Flask(__name__)
 app.config.from_object('flask_settings')
 influxdb = get_influxdb()
 mongodb = PyMongo(app)
+# Building the Response instance that is used to form the json structure
+r = CreateResponse()
 
 @app.route('/tarrif/<name>')
 def get_tarrif_details(name):
@@ -31,6 +34,61 @@ def get_current_demand(meter_id):
         return jsonify(result[0]['points'][0][1])
     else:
         return jsonify(result)
+
+@app.route("/api/logout", methods=["POST"])
+def logout():
+    session['logged_in'] = False
+    return r.success()
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    credentials = request.json
+    if credentials['username'] != 'test' or credentials['password'] != 'test':
+        error = 'Invalid username or password'
+        return r.error(error)
+    else:
+        session['logged_in'] = True
+        # TODO: Updaate user_id & user_roel from db
+        return r.success({
+            "session_id": session.get('_id'),
+            "user_id": 2,
+            "user_role": "user",
+            "csrf_token": session.get('csrf_token')
+        })
+
+@app.route("/api/performance", methods=["POST"])
+def performance():
+    return r.success({
+        "billing_period": "september",
+        "billing_period_startdate": 1410380825,
+        "billing_period_enddate": 1410380825,
+        "season": "summer",
+        "season_startdate": 1410380825,
+        "season_enddate": 1410380825,
+        "tariff": "AG-5E"
+    })
+
+@app.route("/api/powerview", methods=["POST"])
+def powerview():
+    return r.success({
+        "billing_period": "september",
+        "billing_period_startdate": 1410380825,
+        "billing_period_enddate": 1410380825,
+        "season": "summer",
+        "season_startdate": 1410380825,
+        "season_enddate": 1410380825,
+        "period": "peak",
+        "energy_charge": 0.19,
+        "demand_charge": 21.46,
+        "current_demand": 1254,
+        "current_demand_startdate": 1410380825,
+        "current_demand_enddate": 1410380825,
+        "max_demand": 1455,
+        "max_demand_startdate": 1410380825,
+        "max_demand_enddate": 1410380825,
+        "power_factor": 0.9
+    })
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
