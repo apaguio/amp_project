83d9028e344fae48cf5b1642cb60eea91bc4ad37
Diaa Kasem <me@diaa.me>
2014-09-24 02:06:11 +0300

    Adding power factor line graph

--- www/app/scripts/directives/powergraph.js
+++ www/app/scripts/directives/powergraph.js
@@ -14,12 +14,18 @@
         scope.margin = {top: 6, right: 0, bottom: 20, left: 40};
         scope.width = el.width() - scope.margin.right - scope.margin.left;
         scope.height = el.height() - scope.margin.top - scope.margin.bottom;
+        scope.bigheight = scope.height * 2 / 3;
+        scope.smallheight = scope.height / 3;
 
         scope.x = d3.time.scale()
             .range([0, scope.width]);
 
         scope.y = d3.scale.linear()
-            .range([scope.height, 0]);
+            .range([scope.bigheight, 0]);
+
+        // Power Factor Y
+        scope.pfy = d3.scale.linear()
+            .range([scope.smallheight, 0]);
 
         scope.linePower = d3.svg.line()
             //.interpolate("basis")
@@ -31,11 +37,22 @@
             .x(function(d) { return scope.x(d.time); })
             .y(function(d) { return scope.y(d.P - d.S); });
 
+        scope.linePF = d3.svg.line()
+            //.interpolate("basis")
+            .x(function(d) { return scope.x(d.time); })
+            .y(function(d) { return scope.pfy(d.L1_PF); });
+
         scope.svg = d3.select(el[0]).append("svg")
             .attr("width", scope.width + scope.margin.left + scope.margin.right)
-            .attr("height", scope.height + scope.margin.top + scope.margin.bottom)
-        .append("g")
+            .attr("height", scope.height + scope.margin.top + scope.margin.bottom);
+
+        scope.big = scope.svg.append("g")
+            .attr("class", "big")
             .attr("transform", "translate(" + scope.margin.left + "," + scope.margin.top + ")");
+
+        scope.small = scope.svg.append("g")
+            .attr("class", "small")
+            .attr("transform", "translate(" + scope.margin.left + "," + (scope.margin.top + scope.bigheight) + ")");
 
         scope.svg.append("defs").append("clipPath")
             .attr("id", "clip")
@@ -43,14 +60,23 @@
             .attr("width", scope.width)
             .attr("height", scope.height);
 
-        scope.xAxis = scope.svg.append("g")
+        scope.xAxis = scope.big.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0," + scope.height + ")")
+            .attr("transform", "translate(0," + scope.bigheight + ")")
             .call(scope.x.axis = d3.svg.axis().scale(scope.x).orient("bottom"));
 
-        scope.lines = scope.svg.append("g")
+        scope.smallXAxis = scope.small.append("g")
+            .attr("class", "x axis small")
+            .attr("transform", "translate(0," + scope.smallheight + ")")
+            .call(scope.x.axis);
+
+        scope.lines = scope.big.append("g")
             .attr("clip-path", "url(#clip)")
             .attr("class", "lines");
+
+        scope.pflines = scope.small.append("g")
+            .attr("clip-path", "url(#clip)")
+            .attr("class", "pflines");
     }
 
     function update(scope, data, maxDemand) {
@@ -60,6 +86,10 @@
 
         scope.x.domain([scope.start, scope.end]);
         scope.y.domain([0, maxDemand]);
+        var minPF = _.min(data, 'L1_PF').L1_PF - 0.1;
+        var maxPF = _.max(data, 'L1_PF').L1_PF + 0.1;
+
+        scope.pfy.domain([minPF, maxPF]);
 
         scope.lines.selectAll("path").attr("transform", null);
 
@@ -69,6 +99,8 @@
             //.duration(duration)
             //.ease("linear")
             .call(scope.x.axis);
+
+        scope.smallXAxis.call(scope.x.axis);
 
         //var oldTime = _.min(allData, 'time').time;
         var solarLines = scope.lines.selectAll("path.lineSolar");
@@ -83,7 +115,7 @@
             //.attr("transform", "translate(" + scope.x(oldTime) + ")");
         solarLines.data([data]).exit().remove();
 
-        var powerLines = scope.lines.selectAll("path.linePower")
+        var powerLines = scope.lines.selectAll("path.linePower");
             
         powerLines.data([data]).enter().append("path")
             .attr("class", "linePower")
@@ -100,6 +132,20 @@
             //path.transition()
                 //.attr("transform", "translate(" + x(now - (n - 1) * duration) + ")")
                 //.each("end", tick);
+
+        var pflines = scope.pflines.selectAll("path.linePF");
+            
+        pflines.data([data]).enter().append("path")
+            .attr("class", "linePF")
+            .attr("d", scope.linePF);
+        pflines
+            .attr("d", scope.linePF);
+            //.transition()
+            //.duration(duration)
+            //.ease("linear")
+            //.attr("transform", "translate(" + scope.x(oldTime) + ")");
+        pflines.data([data]).exit().remove();
+
     }
 
     function controller(scope, element) {
