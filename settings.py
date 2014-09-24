5e662076dd7671a8202921a3c7f6743e85820b1d
Sherif Zain <sherifzain@yahoo.com>
2014-09-25 00:26:00 +0200

    Changed: celery default timeone, no need for it at the moment

--- settings.py
+++ settings.py
@@ -14,7 +14,6 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT=['json']
-CELERY_TIMEZONE = 'US/Pacific'
 CELERY_IMPORTS = ["tasks",]
 
 # EKM metering scheduled tasks
