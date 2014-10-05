a2f644824d67d7ddc9e7b1a2ae8e3ee55de52de0
Sherif Zain <sherifzain@yahoo.com>
2014-10-05 15:49:10 +0200

    WIP: Added implementation for energy aggregators (current billing period vs last month vs last year)

---
+++ models/performance.py
@@ -0,0 +1,44 @@
+import time
+from datetime import datetime
+from models import db
+from models.powerview import get_customer_timezone, get_tarrif_details
+from models import influxdb
+from pytz import timezone
+
+def get_energy_data(meter_id):
+    result = dict()
+    utc_now = datetime.utcfromtimestamp(time.time()) # current request time
+    tarrif_data = get_tarrif_details(customer_name='test')
+    customer_tz = timezone(tarrif_data['timezone'])
+    customer_tz_now = customer_tz.fromutc(utc_now)
+    time_diff = customer_tz_now - customer_tz.localize(datetime.strptime(tarrif_data['billing_period_startdate'], '%Y-%m-%d %H:%M:%S'))
+
+    #current billing period query
+    billing_period_query = 'select mean(energy_kwh) from /^%s_energy_1h_\.*/ where time > now() - %ss;' %
+                            (meter_id, time_diff.total_seconds())
+    # until merge('%s_energy_1h_*') with regex works
+    billing_period_query_result = influxdb.query(billing_period_query)
+    energy_kwh_avgs = list()
+    for res in billing_period_query_result:
+        energy_kwh_avgs.append(res['points'][0][1])
+    result['this_month'] = float(sum(energy_kwh_avgs))/len(energy_kwh_avgs) if len(energy_kwh_avgs) > 0 else float('nan')
+
+    #last month query
+    last_month_query = 'select mean(energy_kwh) from /^%s_energy_1h_\.*/ where time < now() - %sd and time > now() - %sd;' %
+                        (meter_id, time_diff.days, time_diff.days + 30)
+    last_month_query_result = influxdb.query(last_month_query)
+    energy_kwh_avgs = list()
+    for res in last_month_query_result:
+        energy_kwh_avgs.append(res['points'][0][1])
+    result['last_month'] = float(sum(energy_kwh_avgs))/len(energy_kwh_avgs) if len(energy_kwh_avgs) > 0 else float('nan')
+
+    #last year query
+    last_year_query = 'select mean(energy_kwh) from /^%s_energy_1h_\.*/ where time < now() - %sd and time > now() - %sd;' %
+                        (meter_id, time_diff.days, time_diff.days + 365)
+    last_year_query_result = influxdb.query(last_year_query)
+    energy_kwh_avgs = list()
+    for res in last_month_query_result:
+        energy_kwh_avgs.append(res['points'][0][1])
+    result['last_year'] = float(sum(energy_kwh_avgs))/len(energy_kwh_avgs) if len(energy_kwh_avgs) > 0 else float('nan')
+
+    return result
