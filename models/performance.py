a7271583dcb4c04e7cd1913f7ab8026850df95cb
Sherif Zain <sherifzain@yahoo.com>
2014-10-05 20:38:11 +0200

    WIP: Added implementation for calculate energy charges

--- models/performance.py
+++ models/performance.py
@@ -80,3 +80,27 @@ def get_demand_data(meter_id):
     result['last_year'] = max(demand_maxs)
 
     return result
+
+def calculate_charges(meter_id, solar_meter_id):
+    result = dict()
+    utc_now = datetime.utcfromtimestamp(time.time()) # current request time
+    tarrif_data = get_tarrif_details(customer_name='test') # TODO replace with current customer_id
+    customer_tz = timezone(tarrif_data['timezone'])
+    customer_tz_now = customer_tz.fromutc(utc_now)
+    time_diff = customer_tz_now - customer_tz.localize(datetime.strptime(tarrif_data['billing_period_startdate'], '%Y-%m-%d %H:%M:%S'))
+
+    energy_query = 'select sum(energy_kwh) from /^%s_energy_1h_\.*/ where time > now() - %ss;' %
+                    (meter_id, time_diff.total_seconds())
+    energy_query_result = influxdb.query(energy_query)
+    customer = db.Customer.objects(name='test').first()
+    energy_charges = 0.0
+    for res in energy_query_result:
+        season = res['name'].split('_')[3]
+        peakperiod = res['name'].split('_')[4]
+        for sn in customer['seasons']:
+            if sn['name'] == season:
+                for pp in season['peak_periods']:
+                    if pp['name'] == peakperiod:
+                        energy_charges += pp['energy_charge'] * res['points'][0][1]
+                        break
+    result['energy_charges'] = energy_charges
