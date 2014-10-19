b77fc6e20c3700b2da803fab38e5912db93452e6
Diaa Kasem <me@diaa.me>
2014-10-19 17:23:40 +0200

    Adding logout confirmation

--- www/app/scripts/directives/nav.js
+++ www/app/scripts/directives/nav.js
@@ -1,7 +1,18 @@
 'use strict';
 (function() {
 
-    function controller($rootScope, scope, Auth, AuthEvents, location) {
+    function ModalInstanceController($scope, $modalInstance) {
+
+        $scope.ok = function () {
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    }
+
+    function controller($rootScope, scope, Auth, AuthEvents, location, $modal) {
 
         scope.visible = Auth.isAuthenticated();
 
@@ -14,13 +25,25 @@
         });
 
         scope.logout = function() {
-            Auth.logout();
+
+            var modalInstance = $modal.open({
+                templateUrl: 'logoutModal.html',
+                controller: 'ModalInstanceCtrl'
+            });
+
+            modalInstance.result.then(function() {
+                Auth.logout();
+            }, function() {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+
         };
     }
 
+    angular.module('insightApp').controller('ModalInstanceCtrl', ModalInstanceController);
     angular.module('insightApp')
     .directive('nav', function () {
-        var dependencies = ['$rootScope', '$scope', 'Auth', 'AuthEvents', '$location', controller]
+        var dependencies = ['$rootScope', '$scope', 'Auth', 'AuthEvents', '$location', '$modal', controller]
         return {
             templateUrl: 'views/directives/nav.html',
             restrict: 'E',
