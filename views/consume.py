923fb64b7e65fc0c8eee1f0609f2ed5929f7f2f6
adept <yd.adept@gmail.com>
2015-09-09 13:17:39 -0400

    Use the device address as the meter ID. Added code to fake an extra meter if only one meter in use.

--- views/consume.py
+++ views/consume.py
@@ -4,11 +4,11 @@ from models import db, influxdb
 from datetime import datetime
 from redis import Redis
 from meter_settings import SOLAR_METER_ID, CONSUMPTION_METER_ID
+import pprint
 
 PSUEDO_FEED = False
 
 if PSUEDO_FEED:
-    import pprint
     import time
     import random
 
@@ -16,7 +16,42 @@ redis = Redis()
 
 consume_app = Blueprint('consume_app', __name__)
 
-def log_data_received(xml_data):
+def logit(data, request):
+    fout = open('/tmp/consume_output.txt', 'a')
+    fout.write('Attempt to show raw body\n')
+    try:
+        fout.write(str(data))
+        fout.write('\n')
+        pprint.pprint([(attr, value) for attr, value in data.__dict__.iteritems()], fout)
+        fout.write('\nDone!')
+    except:
+        pass
+    fout.write('\n\nAttempt to print the request object\n')
+    try:
+        pprint.pprint([(attr, value) for attr, value in request.__dict__.iteritems()], fout)
+        fout.write('\nDone!')
+    except:
+        pass
+    fout.write('\n\nAttempt to print the form fields\n')
+    try:
+        pprint.pprint(request.form, fout)
+        fout.write('\nDone!')
+    except:
+        pass
+    fout.write('\n\nAttempt to print the file data\n')
+    try:
+        # Get the FileStorage instance from request
+        file = request.files['LOGFILE']
+        filename = file.filename
+        data = file.read()
+        fout.write(data)
+        fout.write('\nDone!')
+    except:
+        pass
+    fout.flush()
+    fout.close()
+
+def log_data_received(xml_data, FAKE_SECOND_METER=False):
     root = ET.fromstring(xml_data)
     a8810_serial_number = ''
     a8810_name = ''  
@@ -55,8 +90,9 @@ def log_data_received(xml_data):
             # setup data structure for consumption meter
             data2 = {'name': CONSUMPTION_METER_ID, 'columns': ['time', 'P', 'L1_PF', 'L1_V'], 'points': []}
         else:
-            data = {'name': device_name, 'columns': ['time', 'P', 'L1_PF', 'L1_V'], 'points': []}
-        
+            data = {'name': device_address, 'columns': ['time', 'P', 'L1_PF', 'L1_V'], 'points': []}
+            if FAKE_SECOND_METER:
+                data = {'name': CONSUMPTION_METER_ID, 'columns': ['time', 'P', 'L1_PF', 'L1_V'], 'points': []}
         # For each device node, scan for records
         records = device.findall('./records/record')
         for i, record in enumerate(records):
@@ -89,6 +125,8 @@ def log_data_received(xml_data):
                         power2 = long(random.randint(500, 600))
                     else:
                         power = long(float(data_point.get('value'))) * 1000 # converted to watts like EKM meters provide
+                        if FAKE_SECOND_METER:
+                            power += 50
                 # extract power factor value
                 if data_point.get('number') == '12' and data_point.get('name') == 'Power Factor, 3-Ph total':
                     if PSUEDO_FEED:
@@ -135,6 +173,10 @@ def log_data_received(xml_data):
             # store consumption data
             influxdb.write_points([data2])
         else:
+            #f = open('/tmp/a8810_data.txt', 'a')
+            #pprint.pprint(data, f)
+            #f.close()
+            
             # store meter data
             influxdb.write_points([data])
 
@@ -147,14 +189,19 @@ def consume_a8810_data():
             if PSUEDO_FEED:
                 import a8810_data
                 log_data_received(a8810_data.get_data())
-                return 'Success'
+                return 'SUCCESS'
             else:
                 data = request.get_data()
-                form = request.form
-                for field in form:
-                    print 'The value of {field} is "{value}"'.format(field=field, value=form[field])
+                #logit(data, request)
                 log_data_received(data)
-                return 'Success'
+
+                #
+                # Uncomment the following line if only one meter in use
+                #
+
+                #log_data_received(data, FAKE_SECOND_METER=True)
+
+                return 'SUCCESS'
         except:
             return 'Bad'
     else:
